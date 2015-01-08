88eb6f412550073488d2c536a6b5b5669f44265b
Simon Gough <scgough@users.noreply.github.com>
2015-01-08 14:33:14 +0000

    Update ViewController.swift
    comment edit

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Moss Palmer on 08/01/2015.
 //  Copyright (c) 2015 Once Byten. All rights reserved.
 //
-//Simon added a comment.
+//Simon edited a comment.
 import UIKit
 
 class ViewController: UIViewController {
